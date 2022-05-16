45d089b551c4b7d049b3e2fadab00dd02a0c836f
vevenom <92.sinisa@gmail.com>
2022-05-16 10:56:12 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+print(find_packages())
+
+setup(name='MonteScene',
+version='0.1',
+description='MonteScene package',
+url='#',
+author='vevenom',
+author_email='sinisa.stekovic@icg.tugraz.at',
+license='MIT',
+packages=find_packages(),
+zip_safe=False)
