5e1396fe7702f8ceff1744dc1f1b5c0e6ac4bb8c
vevenom <92.sinisa@gmail.com>
2022-05-17 16:20:01 +0200

    Added NodeScore

--- MonteScene/Tree/Node/NodeScore.py
+++ MonteScene/Tree/Node/NodeScore.py
@@ -3,8 +3,17 @@ import numpy as np
 from ..constants import ScoreModes
 
 class NodeScore(object):
+    """
+        Class used for tracking Node score.
+
+        Attributes:
+            max_score: Max. score
+            min_score: Max. score
+            score_sum: Sum of scores
+    """
     def __init__(self):
         self.max_score = -np.inf
+        self.min_score = np.inf
         self.score_sum = 0
 
     def get_score(self, score_mode):
@@ -18,5 +27,7 @@ class NodeScore(object):
     def update_score(self, score):
         if score > self.max_score:
             self.max_score = score
+        if score < self.min_score:
+            self.min_score = score
 
         self.score_sum += score
