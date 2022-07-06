6b4bc1b525da91a31e8c28d847f64be923bb91b1
vevenom <92.sinisa@gmail.com>
2022-07-06 18:16:33 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from MonteScene.Game import Game
+from MonteScene.ProposalGame import ProposalGame
 from MonteScene.MonteCarloTreeSearch import MCTSLogger
 from MonteScene.MonteCarloTreeSearch import MonteCarloSceneSearch
 
 if __name__ == '__main__':
         # Create Game
         # TODO Abstract Class. Define your own Game inherited from this and implement the abstract methods
-        game = Game()
+        game = ProposalGame()
 
         # Create Logger
         mcts_logger = MCTSLogger(game) # TODO Abstract Class. Define your own Logger inherited from this and implement the abstract methods
